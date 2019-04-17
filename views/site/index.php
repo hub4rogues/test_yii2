48e16597042acfaa40d849fc975b40cb179caf71
n37w1z <n37w1z@protonmail.com>
2019-04-17 10:12:58 +0300

    -_-

---
+++ views/site/index.php
@@ -0,0 +1,28 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Hi,</h1>
+
+        <p class="lead">this is my app</p>
+
+        <p><a class="btn btn-lg btn-success" href="/site/app">My App</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+           <h2>Just4fun</h2>
+
+           <p>This is the only reason of existing</p>
+
+           <p><a class="btn btn-default" href="/site/app">another link to the app &reg;</a></p>
+        </div>
+
+    </div>
+</div>
